9f256aadc69d38c23ca2006e5e9058abba0e8c23
maryja-harshunova <spiakota@outlook.com>
2024-02-20 15:27:54 +0500

    add files

---
+++ script.js
@@ -0,0 +1,19 @@
+let soundButtons = document.querySelectorAll(".pads-wrapper > div");
+let sounds = document.querySelectorAll("audio");
+
+for (let i = 0; i < soundButtons.length; i += 1) {
+    soundButtons[i].addEventListener("click", playSound(i));
+}
+
+function playSound(item) {
+    return function () {
+        if (sounds[item].loop) {
+            sounds[item].pause();
+            sounds[item].loop = false;
+        } else {
+            sounds[item].currentTime = 0;
+            sounds[item].play();
+            sounds[item].loop = true;
+        }
+    }
+}
